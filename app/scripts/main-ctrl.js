5785edea91d8caf6ae8e08e2998db92e6e6d006b
kosich <kosich@gmail.com>
2015-02-22 18:09:37 +0200

    Bootstrapp.css'ed, added filter, add button status

--- app/scripts/main-ctrl.js
+++ app/scripts/main-ctrl.js
@@ -2,18 +2,27 @@ angular.module( 'app' )
 .controller( 'main', [ '$scope', 'notesStorage', function( $scope, notesStorage ){
     'use strict';
 
+    var notes = [];
     $scope.$watch( function(){ return notesStorage.keys; }, function(){
-        $scope.files = notesStorage.keys;
+        $scope.notes = notes = notesStorage.keys;
     }, true );
 
+    var note = $scope.note = {};
+
+    Object.defineProperty(this, 'overwriting', { 
+        get : function(){
+            return note.name in notes;
+        } 
+    });
+
     this.save = function( ){
-        console.log( $scope.name, $scope.text );
-        notesStorage.set( $scope.name, $scope.text );
+        console.log( note.name, note.text );
+        notesStorage.set( note.name || '[no title]', note.text || '' );
     };
 
     this.open = function( name ){
-        $scope.name = name;
-        $scope.text = notesStorage.get( name );
+        note.name = name;
+        note.text = notesStorage.get( name );
     };
 
     this.remove = function( name ){
